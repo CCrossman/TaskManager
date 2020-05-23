ca73e46949db857c35036bf60809aaf5d3143a50
ccrossman <christopher.j.crossman@gmail.com>
2020-05-23 13:33:13 -0700

    - Expand tree on adding new element. - Use radio button to select where we insert new element. - Check box to indicate completion of that element.

--- src/main/java/com/crossman/manager/TaskManagerApp.java
+++ src/main/java/com/crossman/manager/TaskManagerApp.java
@@ -1,39 +1,86 @@
 package com.crossman.manager;
 
-import com.sun.source.tree.Tree;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TaskManagerApp extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/application.fxml"));
 		stage.setTitle("Task Manager");
-		stage.setScene(new Scene(root,300,275));
+		stage.setScene(new Scene(root,600,550));
 		stage.show();
 
 		Button btnAddTask = (Button)root.lookup("#btnAddTask");
 		TextField txtAddTask = (TextField)root.lookup("#txtAddTask");
 		TreeView treeView = (TreeView)root.lookup("#treeView");
-		btnAddTask.setOnAction($ -> {
+
+		ToggleGroup toggleGroup = new ToggleGroup();
+		AtomicReference<TreeItem> focus = new AtomicReference<>();
+
+		btnAddTask.setOnAction($1 -> {
 			final String text = txtAddTask.getText();
 			if (text != null && !text.isEmpty()) {
 				System.err.println("Clicked! Task addition: " + text);
 				txtAddTask.clear();
 
-				TreeItem item = new TreeItem(text);
-				treeView.getRoot().getChildren().add(item);
+				final CheckBox checkBox = new CheckBox();
+				checkBox.setOnAction($2 -> {
+					System.err.println("Clicked! Now " + (checkBox.isSelected() ? "selected" : "unselected"));
+				});
+
+				final RadioButton radioButton = new RadioButton();
+				radioButton.setToggleGroup(toggleGroup);
+
+				final TreeItem item = new CheckBoxTreeItem(text, new HBox(radioButton,checkBox));
+				final TreeItem focusedTreeItem = focus.get();
+				focusedTreeItem.getChildren().add(item);
+				expandTree(focusedTreeItem);
+
+				// mark a tree item as the focus
+				radioButton.setOnAction($2 -> {
+					System.err.println("Clicked radio button.");
+					focus.set(item);
+				});
 			}
 		});
 
-		TreeItem rootItem = new TreeItem("Tasks");
+		final CheckBox checkBox = new CheckBox();
+		checkBox.setOnAction($2 -> {
+			System.err.println("Clicked! Now " + (checkBox.isSelected() ? "selected" : "unselected"));
+		});
+
+		final RadioButton radioButton = new RadioButton();
+		radioButton.setToggleGroup(toggleGroup);
+
+		TreeItem rootItem = new CheckBoxTreeItem("Tasks", new HBox(radioButton,checkBox));
 		treeView.setRoot(rootItem);
+		focus.set(rootItem);
+
+		// mark a tree item as the focus
+		radioButton.setOnAction($2 -> {
+			System.err.println("Clicked root radio button.");
+			focus.set(rootItem);
+		});
+		radioButton.setSelected(true);
+	}
+
+	private static void expandTree(TreeItem item) {
+		if (item != null) {
+			item.setExpanded(true);
+
+			for (Object child : item.getChildren()) {
+				if (child instanceof TreeItem) {
+					expandTree((TreeItem)child);
+				}
+			}
+		}
 	}
 }
