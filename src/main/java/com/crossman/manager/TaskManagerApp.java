ca3ab19c17a8291e4fcf47ee9b1cb42cd6ecb15f
ccrossman <christopher.j.crossman@gmail.com>
2020-05-24 13:35:35 -0700

    - Added Delete button-link. - Pressing Enter also submits text field. - Cleaned up code a bit.

--- src/main/java/com/crossman/manager/TaskManagerApp.java
+++ src/main/java/com/crossman/manager/TaskManagerApp.java
@@ -5,12 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class TaskManagerApp extends Application {
+
+	private final ToggleGroup toggleGroup = new ToggleGroup();
+	private final AtomicReference<TreeItem> focus = new AtomicReference<>();
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("/application.fxml"));
@@ -22,33 +27,21 @@ public class TaskManagerApp extends Application {
 		TextField txtAddTask = (TextField)root.lookup("#txtAddTask");
 		TreeView treeView = (TreeView)root.lookup("#treeView");
 
-		ToggleGroup toggleGroup = new ToggleGroup();
-		AtomicReference<TreeItem> focus = new AtomicReference<>();
-
 		btnAddTask.setOnAction($1 -> {
 			final String text = txtAddTask.getText();
 			if (text != null && !text.isEmpty()) {
-				System.err.println("Clicked! Task addition: " + text);
 				txtAddTask.clear();
+				initializeTreeItem(text);
+			}
+		});
 
-				final CheckBox checkBox = new CheckBox();
-				checkBox.setOnAction($2 -> {
-					System.err.println("Clicked! Now " + (checkBox.isSelected() ? "selected" : "unselected"));
-				});
-
-				final RadioButton radioButton = new RadioButton();
-				radioButton.setToggleGroup(toggleGroup);
-
-				final TreeItem item = new CheckBoxTreeItem(text, new HBox(radioButton,checkBox));
-				final TreeItem focusedTreeItem = focus.get();
-				focusedTreeItem.getChildren().add(item);
-				expandTree(focusedTreeItem);
-
-				// mark a tree item as the focus
-				radioButton.setOnAction($2 -> {
-					System.err.println("Clicked radio button.");
-					focus.set(item);
-				});
+		txtAddTask.setOnKeyPressed(ke -> {
+			if (ke.getCode() == KeyCode.ENTER) {
+				final String text = txtAddTask.getText();
+				if (text != null && !text.isEmpty()) {
+					txtAddTask.clear();
+					initializeTreeItem(text);
+				}
 			}
 		});
 
@@ -70,6 +63,37 @@ public class TaskManagerApp extends Application {
 			focus.set(rootItem);
 		});
 		radioButton.setSelected(true);
+	}
+
+	private void initializeTreeItem(String text) {
+		System.err.println("Clicked! Task addition: " + text);
+
+		final CheckBox checkBox = new CheckBox();
+		checkBox.setOnAction($2 -> {
+			System.err.println("Clicked! Now " + (checkBox.isSelected() ? "selected" : "unselected"));
+		});
+
+		final RadioButton radioButton = new RadioButton();
+		radioButton.setToggleGroup(toggleGroup);
+
+		final Hyperlink deleteButton = new Hyperlink("Delete");
+
+		final TreeItem item = new CheckBoxTreeItem(text, new HBox(radioButton,checkBox,deleteButton));
+		final TreeItem focusedTreeItem = focus.get();
+		focusedTreeItem.getChildren().add(item);
+		expandTree(focusedTreeItem);
+
+		// mark a tree item as the focus
+		radioButton.setOnAction($2 -> {
+			System.err.println("Clicked radio button.");
+			focus.set(item);
+		});
+
+		// delete the item and its children
+		deleteButton.setOnAction($2 -> {
+			System.err.println("Clicked delete button.");
+			focusedTreeItem.getChildren().remove(item);
+		});
 	}
 
 	private static void expandTree(TreeItem item) {
